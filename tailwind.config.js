f46fcc8445a0707426a7f4ef2eb37789cd7999fc
Joana <joanasmnho83310@gmail.com>
2022-07-04 14:47:44 +0200

    add fonts and hero html css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ['sf-pro', 'sans-serif'],
+      sans: ['sackers', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
