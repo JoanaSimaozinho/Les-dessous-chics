02c34f89081a433a992c306f29497a64799fffb2
maxbaile <hello@maxbaile.com>
2022-07-13 20:58:20 +0200

    Slider

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-import './sass/app.sass'
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./sass/app.sass";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 2,
+  spaceBetween: 5,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
