6b9210854bd0bb08283a16cd28e0536b61da760a
maxbaile <hello@maxbaile.com>
2022-07-15 17:46:22 +0200

    Header

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./sass/app.sass";
 import Headroom from "headroom.js";
+import "bootstrap";
 
 const header = document.querySelector(".navbar")!;
 
@@ -11,47 +12,6 @@ const headroom = new Headroom(header);
 
 headroom.init();
 
-class Navbar {
-  navbar: HTMLElement;
-  mobileMenu: HTMLElement;
-
-  constructor() {
-    this.navbar = document.querySelector(".navbar")!;
-    this.mobileMenu = document.querySelector(".mobile-menu")!;
-
-    const headroom = new Headroom(this.navbar);
-
-    headroom.init();
-
-    this.bindEvents();
-  }
-
-  bindEvents = () => {
-    this.navbar.querySelectorAll(".navbar-toggler").forEach((toggler) => {
-      toggler.addEventListener("click", this.handleMenu.bind(this));
-    });
-    this.mobileMenu
-      .querySelector(".mobile-menu-overlay")!
-      .addEventListener("click", this.handleMenu.bind(this));
-  };
-
-  handleMenu = () => {
-    console.log("click");
-    const isOpen = this.mobileMenu.classList.contains("open");
-
-    if (isOpen) {
-      this.mobileMenu.classList.remove("open");
-      setTimeout(() => {
-        this.mobileMenu.style.display = "none";
-      }, 300);
-    } else {
-      this.mobileMenu.style.display = "";
-      setTimeout(() => {
-        this.mobileMenu.classList.add("open");
-      }, 100);
-    }
-  };
-}
 class Dropdowns {
   dropdown: HTMLElement;
   toggler: HTMLButtonElement | HTMLAnchorElement;
@@ -85,7 +45,6 @@ class Dropdowns {
   };
 }
 
-new Navbar();
 document
   .querySelectorAll<HTMLElement>(".dropdown")
   .forEach((dropdown) => new Dropdowns(dropdown));
