f2a71f1132c11de261962de26997777e3975e98c
maxbaile <hello@maxbaile.com>
2022-07-18 16:46:46 +0200

    Header update

--- src/components/collapse.ts
+++ src/components/collapse.ts
@@ -6,14 +6,23 @@ import { reflow } from "../utils/index";
 const CLASS_NAME_SHOW = "show";
 const CLASS_NAME_COLLAPSE = "collapse";
 const CLASS_NAME_COLLAPSING = "collapsing";
+const CLASS_NAME_TOGGLED = "toggled";
+
+const SELECTOR_DATA_TOGGLE = '[data-toggle="collapse"]';
 
 export default class Collapse {
   private element: HTMLElement;
   private isTransitioning: boolean;
+  private toggler: HTMLElement;
 
   constructor(element: HTMLElement) {
     this.element = element;
+    this.toggler = document.querySelector(
+      `[data-target="#${this.element.id}"]`
+    );
     this.isTransitioning = false;
+
+    console.log(this.toggler);
   }
 
   public toggle() {
@@ -33,6 +42,8 @@ export default class Collapse {
     this.element.classList.add(CLASS_NAME_COLLAPSING);
 
     this.element.style.height = "0px";
+
+    this.addAriaAndToggledClass(true);
 
     this.isTransitioning = true;
 
@@ -64,6 +75,8 @@ export default class Collapse {
     this.element.classList.add(CLASS_NAME_COLLAPSING);
     this.element.classList.remove(CLASS_NAME_COLLAPSE, CLASS_NAME_SHOW);
 
+    this.addAriaAndToggledClass(false);
+
     this.isTransitioning = true;
 
     const complete = () => {
@@ -80,5 +93,10 @@ export default class Collapse {
 
   private isShown(element = this.element) {
     return element.classList.contains(CLASS_NAME_SHOW);
+  }
+
+  private addAriaAndToggledClass(isOpen: boolean) {
+    this.toggler.classList.toggle(CLASS_NAME_TOGGLED);
+    this.toggler.setAttribute("aria-expanded", `${isOpen}`);
   }
 }
